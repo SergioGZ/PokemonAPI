6c68ebdd41f940e8b54363665da560e9e3f8f581
Sergio González <sergiogg97@hotmail.com>
2024-06-07 21:03:55 +0200

    Fixing paddings

--- src/components/PokemonCards.jsx
+++ src/components/PokemonCards.jsx
@@ -4,20 +4,20 @@ function PokemonCards() {
 
   return (
     <>
-      <div className='container py-4'>
-        <div className='row pt-2'>
-          <div className='col-md-4 offset-md-2 col-sm-12'>
+      <div className='container py-3'>
+        <div className='row'>
+          <div className='col-md-4 offset-md-2 col-sm-12 pb-2'>
             <PokemonAPI />
           </div>
-          <div className='col-md-4 col-sm-12'>
+          <div className='col-md-4 col-sm-12 pb-2'>
             <PokemonAPI />
           </div>
         </div>
-        <div className='row pt-2'>
-          <div className='offset-md-2 col-md-4 col-sm-12'>
+        <div className='row'>
+          <div className='offset-md-2 col-md-4 col-sm-12 pb-2'>
             <PokemonAPI />
           </div>
-          <div className='col-md-4 col-sm-12'>
+          <div className='col-md-4 col-sm-12 pb-2'>
             <PokemonAPI />
           </div>
         </div>
